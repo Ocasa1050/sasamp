8d4f0bc7db601b9d38c7acc049abe7fa8527e8f2
Flos949494 <62321937-flos949494@users.noreply.replit.com>
2026-09-20 00:02:35 +0000

    Bypass native game file check callback

--- app/src/main/cpp/multiplayer/java_systems/GameFilesCheck.cpp
+++ app/src/main/cpp/multiplayer/java_systems/GameFilesCheck.cpp
@@ -3,25 +3,37 @@
 //
 
 #include "GameFilesCheck.h"
-#include "../util/CJavaWrapper.h"
 #include "raknet/BitStream.h"
 #include "net/netgame.h"
 
-void CGameFilesCheck::RequestChecked() {
-    auto env = g_pJavaWrapper->GetEnv();
-    auto clazz = env->GetObjectClass(g_pJavaWrapper->activity);
+namespace {
+void SendGameFilesCheckResult(bool ok) {
+    if (pNetGame == nullptr || pNetGame->GetRakClient() == nullptr) {
+        return;
+    }
 
-    auto method = env->GetStaticMethodID(clazz, "requestGameFilesCheck", "()V");
-    env->CallStaticVoidMethod(clazz, method);
+    RakNet::BitStream bsSend;
+    bsSend.Write((uint8_t) ID_CUSTOM_RPC);
+    bsSend.Write((uint8_t) RPC_REQUEST_CHECK_FILES);
+    bsSend.Write((uint8_t) ok);
+
+    pNetGame->GetRakClient()->Send(
+            &bsSend,
+            MEDIUM_PRIORITY,
+            RELIABLE_SEQUENCED,
+            0
+    );
+}
+}
+
+void CGameFilesCheck::RequestChecked() {
+    // File downloading/checking is temporarily disabled. Reply directly so
+    // the server can continue without entering the Java callback path.
+    SendGameFilesCheckResult(true);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_russia_game_core_Samp_00024Companion_gameFilesChecked(JNIEnv *env, jobject clazz, jboolean ok) {
-    RakNet::BitStream bsSend;
-    bsSend.Write((uint8_t)  ID_CUSTOM_RPC);
-    bsSend.Write((uint8_t)  RPC_REQUEST_CHECK_FILES);
-    bsSend.Write((uint8_t)  ok);
-
-    pNetGame->GetRakClient()->Send(&bsSend, MEDIUM_PRIORITY, RELIABLE_SEQUENCED, 0);
+    SendGameFilesCheckResult(ok == JNI_TRUE);
 }
